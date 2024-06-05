03e20376563236e93b16b51595c18a04a732610c
mohamed rego <mohamedmrslan@gmail.com>
2024-06-06 00:00:23 +0300

    updated

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ cloudinary.config({
 })
 
 //middlewares
-app.use(express.json({limit: "500mb"}));
+app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true })); //To parse form data in the req.body
 app.use(cookieParser());
 
