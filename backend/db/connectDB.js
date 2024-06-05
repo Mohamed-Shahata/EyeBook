0b3966381ab860d5888b39fe0c3c4d696c0eb42d
mohamed rego <mohamedmrslan@gmail.com>
2024-06-05 23:58:43 +0300

    عحيشفث

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{
-      useNewUrlParser: true,
-			useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB connected`)
   } catch (error) {
