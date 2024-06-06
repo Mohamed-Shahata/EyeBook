73f57f18bc0e9155405ede50c432597ea19eec3f
mohamed rego <mohamedmrslan@gmail.com>
2024-06-06 23:43:01 +0300

    updated

--- frontend/src/components/SuggestedUser.jsx
+++ frontend/src/components/SuggestedUser.jsx
@@ -8,36 +8,39 @@ const SuggestedUser = ({ user }) => {
 
 	return (
 		<Flex gap={2} justifyContent={"space-between"} alignItems={"center"} mb={2}>
-			{/* left side */}
-			<Flex gap={2} as={Link} to={`${user.username}`}>
-				<Avatar src={user.profilePic} name={user.name}/>
-				<Box>
-					<Flex minW={32}>
-						<Text fontSize={"sm"} fontWeight={"bold"}>
-						{user.name}
-						</Text>
-            {/* {user.role == "USER" ? "" : <Image src="/verified.png" w={5} h={5} ml={2} my={1}/>} */}
+
+		{user.isFrozen ? null : (
+			<>
+										<Flex gap={2} as={Link} to={`${user.username}`}>
+						<Avatar src={user.profilePic} name={user.name}/>
+						<Box>
+							<Flex minW={32}>
+								<Text fontSize={"sm"} fontWeight={"bold"}>
+								{user.name}
+								</Text>
+								{/* {user.role == "USER" ? "" : <Image src="/verified.png" w={5} h={5} ml={2} my={1}/>} */}
+							</Flex>
+							<Text color={"gray.light"} fontSize={"sm"}>
+								{user.username}
+							</Text>
+						</Box>
 					</Flex>
-					<Text color={"gray.light"} fontSize={"sm"}>
-						{user.username}
-					</Text>
-				</Box>
-			</Flex>
-			{/* right side */}
-			<Button
-				ml={1}
-				size={"sm"}
-				color={following ? "black" : "white"}
-				bg={following ? "white" : "blue.400"}
-				onClick={handleFollowUnfollow}
-				isLoading={updating}
-				_hover={{
-					color: following ? "black" : "white",
-					opacity: ".8",
-				}}
-			>
-				{following ? "Unfollow" : "Follow"}
-			</Button>
+					<Button
+						ml={1}
+						size={"sm"}
+						color={following ? "black" : "white"}
+						bg={following ? "white" : "blue.400"}
+						onClick={handleFollowUnfollow}
+						isLoading={updating}
+						_hover={{
+							color: following ? "black" : "white",
+							opacity: ".8",
+						}}
+					>
+						{following ? "Unfollow" : "Follow"}
+					</Button>
+			</>
+		)}
 		</Flex>
 	);
 };
